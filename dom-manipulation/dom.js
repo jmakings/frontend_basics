5293de2b843e0d1bad35052abed92dabe89f58d8
Jeff Makings <jeffmakings@gmail.com>
2023-01-29 18:35:19 -0800

    elements created and added to container

--- dom-manipulation/dom.js
+++ dom-manipulation/dom.js
@@ -1,9 +1,31 @@
 
+const container = document.querySelector("#container");
+
+// paragraph creation 
 const p1 = document.createElement('p');
 p1.style.color = 'red';
-document.body.appendChild(p1);
+p1.textContent = "Hey I'm red!"; 
+container.appendChild(p1);
 
+// h3 creation and addition
 const h3_1 = document.createElement('h3');
 h3_1.style.color = 'blue';
-document.body.appendChild(h3_1); 
+h3_1.textContent = "I'm a blue h3!"; 
+container.appendChild(h3_1); 
+
+// div with elements inside
+const div1 = document.createElement('div'); 
+div1.style.borderColor = "black"; 
+//document.body.appendChild(div1);
+
+const h1_1 = document.createElement('h1'); 
+h1_1.textContent = "I'm in a div";
+div1.appendChild(h1_1); 
+
+const p2 = document.createElement('p');
+p2.textContent = "ME TOO!"; 
+div1.appendChild(p2); 
+
+container.appendChild(div1);
+
 
