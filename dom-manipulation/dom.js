fc5c91c8c128506ba6ae48855aa93fffe549a32a
Jeff Makings <jeffmakings@gmail.com>
2023-01-29 18:09:30 -0800

    intial commit

---
+++ dom-manipulation/dom.js
@@ -0,0 +1,9 @@
+
+const p1 = document.createElement('p');
+p1.style.color = 'red';
+document.body.appendChild(p1);
+
+const h3_1 = document.createElement('h3');
+h3_1.style.color = 'blue';
+document.body.appendChild(h3_1); 
+
